5cd6e5a0e72da7b63de3597d0d5117c31ed6716c
codemuhammed <codemuhammed@gmail.com>
2017-08-31 22:33:12 +0100

    Update mockup for dashboard

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-  title = 'Dashboard';
+  title = 'iDea bOx';
+  ideas = [
+    {
+      'content': 'Just like in Angular 1.x, Angular 2 also provides with its own service called Http for making requests to servers',
+      'impact': 8,
+      'ease': 8,
+      'confidence': 8
+    }
+  ];
+  selectedIdea = this.ideas[0];
 }
